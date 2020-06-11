44228e9e37e11e80135bd4fdaf0207d94da6caee
GerA <lpgera@gmail.com>
2020-06-11 09:56:11 +0200

    dockerize services

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
     app.use(
         '/graphql',
         createProxyMiddleware({
-            target: 'http://localhost:4001/graphql',
+            target: 'http://backend:4001/graphql',
             changeOrigin: true,
         })
     );
